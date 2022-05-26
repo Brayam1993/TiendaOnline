acf01d567968d49dc7cb6baef9e19b3de2b97775
Brayam1993 <argofrog@live.com.mx>
2022-05-26 17:37:58 -0500

    paste once in db

--- app.js
+++ app.js
@@ -21,16 +21,47 @@ app.listen(3000, () => {
     console.log('Server runs on port 3000');
 });
 
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// const fetch = (...args) =>
+//   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const url = 'https://fakestoreapi.com/products/1'
+// const url = 'https://fakestoreapi.com/products/1'
 
-function getData(url, cb) {
-    fetch(url)
-            .then(res=>res.json())
-            .then(result=>cb(result))
-}  
+// function getData(url, cb) {
+//     fetch(url)
+//             .then(res=>res.json())
+//             .then(result=>cb(result))
+// }  
 
-getData(url, (data) => console.log ({ data }))    
-       
+// getData(url, (data) => console.log ({ data }))    
+
+const dataOne = {
+    "pname": "aguacate"
+}
+
+const Product = require('./store.models');
+
+var doneTheStuff;
+
+function whathever(){
+    if (!doneTheStuff) {
+        doneTheStuff = true;
+    try {  
+        createApi = function (res) {
+            const newProduct = new Product(dataOne);
+            if (dataOne === Object && Object.keys(dataOne).length === 0) {
+               res.status(400).send({ error: true, message: 'Please provide all required field'}); 
+            } else {
+                Product.create(newProduct, (err, product) => {
+                    if (err) res.send(err);
+                    //res.json({ error: false, message: 'Product added successfully!', data: product });
+                });
+            }
+        };
+        createApi()                  
+        } catch (ex) {
+            console.log('exception handle ++++', ex);
+        }
+    }
+}
+
+whathever();
