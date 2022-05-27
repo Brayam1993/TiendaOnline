8c853ba0728de3fe3715a0d275bf3fbc2a036b3f
Brayam1993 <argofrog@live.com.mx>
2022-05-26 21:23:32 -0500

    save data in varable that works

--- app.js
+++ app.js
@@ -1,67 +1,74 @@
 
-const express = require ('express');
+// const express = require ('express');
 
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 
-const app = express();
+// const app = express();
 
-const storeRoutes = require('./store.routes')
+// const storeRoutes = require('./store.routes')
 
-app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.urlencoded({ extended: true}));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
-app.use('/store', storeRoutes);
+// app.use('/store', storeRoutes);
 
-app.listen(3000, () => {
-    console.log('Server runs on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server runs on port 3000');
+// });
 
-// const fetch = (...args) =>
-//   import('node-fetch').then(({ default: fetch }) => fetch(...args));
+/* FETCH */
 
-// const url = 'https://fakestoreapi.com/products/1'
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-// function getData(url, cb) {
-//     fetch(url)
-//             .then(res=>res.json())
-//             .then(result=>cb(result))
-// }  
+const url = 'https://fakestoreapi.com/products/1'
 
-// getData(url, (data) => console.log ({ data }))    
-
-const dataOne = {
-    "pname": "aguacate"
+async function fetchData() {
+    let response = await fetch(url);
+    let data = await response.json();
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+    return data;
 }
 
-const Product = require('./store.models');
+let abc = await fetchData();
+console.log(abc,"afuera");
 
-var doneTheStuff;
+/* TEST INSERT DATA TO DB FROM OBJECT*/
 
-function whathever(){
-    if (!doneTheStuff) {
-        doneTheStuff = true;
-    try {  
-        createApi = function (res) {
-            const newProduct = new Product(dataOne);
-            if (dataOne === Object && Object.keys(dataOne).length === 0) {
-               res.status(400).send({ error: true, message: 'Please provide all required field'}); 
-            } else {
-                Product.create(newProduct, (err, product) => {
-                    if (err) res.send(err);
-                    //res.json({ error: false, message: 'Product added successfully!', data: product });
-                });
-            }
-        };
-        createApi()                  
-        } catch (ex) {
-            console.log('exception handle ++++', ex);
-        }
-    }
-}
+// const dataOne = {
+//     "pname": "aguacate"
+// }
 
-whathever();
+// const Product = require('./store.models');
+
+// var doneTheStuff;
+
+// function whathever(){
+//     if (!doneTheStuff) {
+//         doneTheStuff = true;
+//     try {  
+//         createApi = function (res) {
+//             const newProduct = new Product(dataOne);
+//             if (dataOne === Object && Object.keys(dataOne).length === 0) {
+//                res.status(400).send({ error: true, message: 'Please provide all required field'}); 
+//             } else {
+//                 Product.create(newProduct, (err, product) => {
+//                     if (err) res.send(err);
+//                     //res.json({ error: false, message: 'Product added successfully!', data: product });
+//                 });
+//             }
+//         };
+//         createApi()                  
+//         } catch (ex) {
+//             console.log('exception handle ++++', ex);
+//         }
+//     }
+// }
+
+// whathever();
